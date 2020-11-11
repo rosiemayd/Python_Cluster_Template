0f7e0168a25c9fad529e575085f3d7039ffe760c
Ayan Sengupta <Ayan.Sengupta@rhul.ac.uk>
2020-11-11 13:40:10 +0000

    Update call_python_script.sh

--- call_python_script.sh
+++ call_python_script.sh
@@ -2,9 +2,10 @@
 
 # The python scripts folder is taken as an input
 script_folder=$1
+subject=$2
 
 source /usr/local/apps/psycapps/config/conda_bash_update
 
 cd $script_folder
 
-python sample_python_script.py
+python sample_python_script.py $subject
