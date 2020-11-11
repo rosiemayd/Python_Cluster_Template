13d237d113d8847aae80ba9e404bfa6aba896129
Ayan Sengupta <Ayan.Sengupta@rhul.ac.uk>
2020-11-11 13:57:49 +0000

    Update sample_python_script.py

--- sample_python_script.py
+++ sample_python_script.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 subject = sys.argv[1]
-print ("Hello World")
+print ("Hello World Subject"+str(subject))
 
 f = open("filename"+str(subject)+".txt", "a")
 f.write("Now the file has more content: subject"+str(subject))
